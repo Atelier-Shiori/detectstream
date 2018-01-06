11b47391f257e6068f3a50b8f2b5bf2a4d666d23
桐間紗路 <kirimasharo@Mac-Pro.local>
2018-01-05 22:31:37 -0500

    Improve Amazon Prime Video stream detection

--- detectstream/MediaStreamParse.m
+++ detectstream/MediaStreamParse.m
@@ -264,7 +264,7 @@
             }
             else if ([site isEqualToString:@"amazon"]) {
                 // Amazon Prime Video/Anime Strike
-                if ([ez checkMatch:url pattern:@"\\/gp\\/video\\/detail\\/*.*"]) {
+                if ([ez checkMatch:url pattern:@"(\\/gp\\/video\\/detail\\/*.*|\\/.+\\/dp\\/.*\\/)"]) {
                     NSString * DOM = [NSString stringWithFormat:@"%@",m[@"DOM"]];
                     if ([DOM isEqualToString:@"(null) - (null)"]) {
                         // Silverlight Player not supported
